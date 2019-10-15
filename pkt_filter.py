e3d7cbd56c18fabf75945b8134c793830a7c4bdb
Scytheroid <bohdan.kopcak@gmail.com>
2019-10-15 10:59:05 +0200

    Added base functions. Able to cut input data and save it into a file

---
+++ pkt_filter.py
@@ -0,0 +1,18 @@
+dataFile = open("./AnalyticData/data.txt", "w")
+pkts = rdpcap("test.pcap")
+src_to_dest = dict()
+
+for pkt in pkts:
+	if pkt[Dot11].addr3 in src_to_dest:
+		src_to_dest.get(pkt[Dot11].addr3).append(pkt[Dot11].addr1)
+	else:
+		src_to_dest[pkt[Dot11].addr3] = [pkt[Dot11].addr1]
+
+dataFile.write(str(src_to_dest))
+
+for src in src_to_dest.keys():
+	print(src)
+	for dest in src_to_dest[src]:
+		print("\t" + str(dest))
+		# print(dest)
+	print()
