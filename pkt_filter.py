c933166ae74df40d810dc18997f137b1437443d8
Scytheroid <bohdan.kopcak@gmail.com>
2019-10-15 11:24:04 +0200

    Script is now aware of source and destination addresses without bugs

--- pkt_filter.py
+++ pkt_filter.py
@@ -3,11 +3,20 @@ pkts = rdpcap("test.pcap")
 src_to_dest = dict()
 
 for pkt in pkts:
-	if pkt[Dot11].type == 2:
-		if pkt[Dot11].addr3 in src_to_dest:
-			src_to_dest.get(pkt[Dot11].addr3).append(pkt[Dot11].addr1)
+	if pkt[Dot11].type == 2 and pkt[Dot11].subtype == 8:
+		src, dest = ""
+		if pkt[Dot11].FCfield & "to-DS":
+			src = pkt[Dot11].addr1
+			dest = pkt[Dot11].addr3
+		elif pkt[Dot11].FCfield & "from-DS":
+			src = pkt[Dot11].addr3
+			dest = pkt[Dot11].addr1
+
+
+		if src in src_to_dest:
+			src_to_dest.get(src).append(dest)
 		else:
-			src_to_dest[pkt[Dot11].addr3] = [pkt[Dot11].addr1]
+			src_to_dest[src] = [dest]
 
 dataFile.write(str(src_to_dest))
 
