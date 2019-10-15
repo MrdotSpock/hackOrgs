ad5175721e2af96f04761803b874f2e375033acd
Scytheroid <bohdan.kopcak@gmail.com>
2019-10-15 13:02:59 +0200

    Resloving indentation issues

--- pkt_filter.py
+++ pkt_filter.py
@@ -31,21 +31,21 @@ def readcap(filename):
     src_to_dest = dict()
 
     for pkt in pkts:
-    if pkt[Dot11].type == 2 and pkt[Dot11].subtype == 8:
-        src = ""
-        dest = ""
+        if pkt[Dot11].type == 2 and pkt[Dot11].subtype == 8:
+            src = ""
+            dest = ""
 
-        if pkt[Dot11].FCfield & 0x1:    #From device to router
-            src = pkt[Dot11].addr1
-            dest = pkt[Dot11].addr3
-        elif pkt[Dot11].FCfield & 0x2:    #From router to device
-            src = pkt[Dot11].addr3
-            dest = pkt[Dot11].addr1
+            if pkt[Dot11].FCfield & 0x1:    #From device to router
+                src = pkt[Dot11].addr1
+                dest = pkt[Dot11].addr3
+            elif pkt[Dot11].FCfield & 0x2:    #From router to device
+                src = pkt[Dot11].addr3
+                dest = pkt[Dot11].addr1
 
-        if src in src_to_dest:
-            src_to_dest.get(src).append(dest)
-        else:
-            src_to_dest[src] = [dest]
+            if src in src_to_dest:
+                src_to_dest.get(src).append(dest)
+            else:
+                src_to_dest[src] = [dest]
     pkts.close()
 return src_to_dest
 
