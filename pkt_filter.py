005ac3930da942ff6b40a8453d450147d7d3d70b
Scytheroid <bohdan.kopcak@gmail.com>
2019-10-15 11:45:47 +0200

    Bug with FCfield was solved and a function for dict printing was created

--- pkt_filter.py
+++ pkt_filter.py
@@ -1,14 +1,24 @@
+# Functions
+def printDict(dict):
+	for src in dict.keys():
+		print(src)
+		for dest in dict[src]:
+			print("\t" + str(dest))
+		print()
+
+# Main
 dataFile = open("./AnalyticData/data.txt", "w")
 pkts = rdpcap("test.pcap")
 src_to_dest = dict()
 
 for pkt in pkts:
 	if pkt[Dot11].type == 2 and pkt[Dot11].subtype == 8:
-		src, dest = ""
-		if pkt[Dot11].FCfield & "to-DS":
+		src = ""
+		dest = ""
+		if pkt[Dot11].FCfield & 0x1: #From device to router
 			src = pkt[Dot11].addr1
 			dest = pkt[Dot11].addr3
-		elif pkt[Dot11].FCfield & "from-DS":
+		elif pkt[Dot11].FCfield & 0x2:	#From router to device
 			src = pkt[Dot11].addr3
 			dest = pkt[Dot11].addr1
 
@@ -20,9 +30,6 @@ for pkt in pkts:
 
 dataFile.write(str(src_to_dest))
 
-for src in src_to_dest.keys():
-	print(src)
-	for dest in src_to_dest[src]:
-		print("\t" + str(dest))
-		# print(dest)
-	print()
+printDict(src_to_dest)
+
+
