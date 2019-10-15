53e873cbf52e940bfb247f1c39e6c06a5bb05473
Scytheroid <bohdan.kopcak@gmail.com>
2019-10-15 12:59:05 +0200

    Made a loading function with same as former body.

--- pkt_filter.py
+++ pkt_filter.py
@@ -26,15 +26,15 @@ def count(dictionary):
         dict_of_names.clear()
     return final_dict
 
-# Main
-dataFile = open("./AnalyticData/data.txt", "w")
-pkts = rdpcap("test.pcap")
-src_to_dest = dict()
+def readcap(filename):
+    pkts = rdpcap("test.pcap")
+    src_to_dest = dict()
 
-for pkt in pkts:
+    for pkt in pkts:
     if pkt[Dot11].type == 2 and pkt[Dot11].subtype == 8:
         src = ""
         dest = ""
+
         if pkt[Dot11].FCfield & 0x1:    #From device to router
             src = pkt[Dot11].addr1
             dest = pkt[Dot11].addr3
@@ -42,12 +42,15 @@ for pkt in pkts:
             src = pkt[Dot11].addr3
             dest = pkt[Dot11].addr1
 
-
         if src in src_to_dest:
             src_to_dest.get(src).append(dest)
         else:
             src_to_dest[src] = [dest]
+    pkts.close()
+return src_to_dest
 
+# Main
+dataFile = open("./AnalyticData/data.txt", "w")
 dataFile.write(str(src_to_dest))
 
 printDict(src_to_dest)
