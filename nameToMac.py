af7500ba2c105686b3b7e27ed0aac79b3dfa6f0b
Unknown <gongputer@gmail.com>
2019-10-16 22:05:27 +0200

    add orgNameToMac dictionary

---
+++ nameToMac.py
@@ -0,0 +1,14 @@
+nameToMac = {
+    "HonHair":"fc:01:7c:0b:bb:5b",
+    "Kristý":"Toshiba (najít)",
+    "Pavel?Azurewav":"40:9f:38:51:51:c5",
+    "Kačka/Fujitsu":"00:19:99:fa:64:e4",
+    "Acer/Liteon":"58:00:e3:df:7f:23",
+    "IntelCor":" 08:d4:0c:b5:9f:4b",
+    "Motorola":"54:27:58:52:a3:7f",
+    "SonyMob":"40:40:a7:ba:13:03",
+    "Xerox":"9c:93:4e:2e:09:87",
+    "Apple(Jane)":"50:7a:c5:09:f4:c8",
+    "SamsungE":"bc:76:5e:f9:1f:68",
+    "HuaweiTe":"06:f0:21:37:0e:91",
+}
