37edddabf29ef355a3673400a73e392f745fe948
Scytheroid <bohdan.kopcak@gmail.com>
2019-10-17 11:03:38 +0200

    Removed some obsolete files and functions.

--- pkt_filter.py
+++ pkt_filter.py
@@ -54,7 +54,7 @@ class Log:
             if src == None:
                 continue
             if src in self.comments:
-                fh.write("{} {}\n".format(src, comments[src]))
+                fh.write("{} ({})\n".format(src, comments[src]))
             else:
                 fh.write("{} ()\n".format(src))
             for fd in self.items[src]:
@@ -88,71 +88,3 @@ class Log:
                     self.comments[currentSrc] = comment
     def addFromPcap(self, filename):
         sniff(offline=filename, store=False, prn=self.insertFrame)
-
-# Functions
-def printDict(dictionary):
-    for src in dictionary.keys():
-        print(src)
-        for dest in dictionary[src]:
-            print("\t" + str(dest))
-        print()
-
-def count(dictionary):
-    dict_of_names = dict()
-    final_dict = dict()
-    for key in dictionary:
-        for addr in dictionary[key]:
-            if addr in dict_of_names:
-                dict_of_names[addr] += 1
-            else:
-                dict_of_names[addr] = 1
-        for i in dict_of_names:
-            if key in final_dict:
-                x = (i, dict_of_names[i])
-                final_dict[key].append(x)
-            else:
-                final_dict[key] = []
-                x = (i, dict_of_names[i])
-                final_dict[key].append(x)
-        dict_of_names.clear()
-    return final_dict
-
-def readcap(filename):
-    pkts = rdpcap("test.pcap")
-    src_to_dest = dict()
-
-    for pkt in pkts:
-        if pkt[Dot11].type == 2 and pkt[Dot11].subtype == 8:
-            src, dest = frameToSrcDest(pkt)
-            if src in src_to_dest:
-                src_to_dest.get(src).append(dest)
-            else:
-                src_to_dest[src] = [dest]
-    return src_to_dest
-
-# Main
-dataFile = open("./AnalyticData/data.txt", "w")
-src_to_dest = readcap("test.pcap")
-print("Let us analyze your PCAP data")
-print("Tell us what you want to do:")
-while True:
-    print(
-        "[1] Load new PCAP file\n" +
-        "[2] Show traffic log\n" +
-        "[3] Match MAC address with a name\n" +
-        "[4] End"
-    )
-    usrChoice = input()
-    if usrChoice == '1':
-        print("What's the name of the file?")
-        readcap(input())
-        print("File loaded")
-    if usrChoice == '2':
-        printDict(count(src_to_dest))
-    if usrChoice == '3':
-        pass
-        ##### YET TO BE DONE #####
-    if usrChoice == '4':
-        exit()
-    print("Task finished.\n")
-    print("Do you want to do something else?")
--- scapyTest.py
+++
@@ -1,9 +0,0 @@
-from scapy.all import *
-import time
-
-pk = rdpcap('test.pcap')
-srcs = set()
-for pkt in pk: 
-    srcs.add(time.ctime(pkt.time))
-
-print(srcs)
